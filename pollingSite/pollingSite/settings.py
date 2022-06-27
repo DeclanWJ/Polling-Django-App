243c7bd0f730dc057095e0717c65b682330aa597
DeclanWJ <declanj2000@hotmail.com>
2022-06-27 13:22:53 -0500

    Covering up secret key
    1. Hide secret key in .gitignore

--- pollingSite/pollingSite/settings.py
+++ pollingSite/pollingSite/settings.py
@@ -20,8 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8vqin!_)0^c-otf4hr0ds_gd31_p7tq%$gsff@!&o7s8pu&1c#'
+from decouple import config
 
+SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
